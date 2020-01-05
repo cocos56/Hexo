06f2023b9c11844386b6a7e049a453140fab9a25
coco <zj175@139.com>
2020-01-05 23:47:22 +0800

    更新于2020年01月05日	23:47:21.10

--- sync_main.py
+++ sync_main.py
@@ -1,11 +1,18 @@
 # -*- coding: UTF-8 -*-
 from sync import run
+import os
 
 if __name__ == "__main__":
+    print("正在生成public")
+    os.system("hexo g")
+    
     li=[
         (r'H:\G\Hexo\public', r'H:\G\coco5666.github.io\blog')
     ]
     
     run(li)
 
+    os.chdir(r"H:\G\coco5666.github.io")
+    os.system("一键更新变动.bat")
+    os.system("start https://github.coco56.top/blog/")
     input("同步已完成")
